b43c7552f48ccfa0716dfd32988c38efe0cda226
tharaga <tharagarealestate@gmail.com>
2025-08-15 23:29:12 +0530

    form link

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Paste your Google Sheets "Publish to web" CSV link here.
 // Example: "https://docs.google.com/spreadsheets/d/e/2PACX-.../pub?output=csv"
 window.CONFIG = {
-  SHEET_CSV_URL: "" // leave blank to use local data.json (for testing)
+  SHEET_CSV_URL: "https://script.google.com/macros/s/AKfycbz2VtbFFldjP52u-aemBTLNqBx31KlRbkXZ5wsXNVF8Ic-6wb1JwsX_s3sAxWpncz3oig/exec" // leave blank to use local data.json (for testing)
 };
